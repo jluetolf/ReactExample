654186cc867cdd4c298e5b8b865d6d10c31ce0cb
Joerg Luetolf <joerg.luetolf@comfone.com>
2019-05-14 13:57:12 +0200

    -

--- orchestration/src/components/Detail/Detail.js
+++ orchestration/src/components/Detail/Detail.js
@@ -6,13 +6,17 @@ const Detail = (props) => {
     const details = (
         {
             "verdict": "Successful",
-            "comment": "Full Orchestration completed succefully",
+            "comment": "Full Orchestration completed successfully",
             "date": "2019_05_11_12-17-52",
             "orchestrationType": "Full",
             "error": null,
             "resultList": [
                 {
+                    "id": "TEST1",
+                    "function": "TEST",
                     "verdict": "Successful",
+                    "order": 1,
+                    "orchestration": "secondary",
                     "dnsResults": [
                         {
                             "verdict": "Successful",
@@ -81,11 +85,14 @@ const Detail = (props) => {
                     ]
                 },
                 {
+                    "id": "DRF_BAU1",
+                    "function": "DRF",
+                    "order": 2,
+                    "orchestration": "secondary",
                     "verdict": "Successful",
                     "code": 0,
                     "serviceName": "orchestration-service-mg-bau1",
                     "server": "csvdf220",
-                    "function": "DRF",
                     "standardOutput": null,
                     "errorOutput": "Warning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\n",
                     "verificationFile": "/home/ultracomp/etc/ListeBlanche.csv",
@@ -94,6 +101,10 @@ const Detail = (props) => {
                     "configuratorLog": "3 csvmg226 nb1[18466]: Stopped\nMay 11 10:19:03 csvmg226 configurator: EXPORT [End of csv generation of ALL configuration files (csv)]\nMay 11 10:19:04 csvmg226 configurator: EXPORT [Check config -> OK]\nMay 11 10:19:05 csvmg226 configurator: EXPORT [Config exported locally]\nMay 11 10:19:05 csvmg226 configurator: EXPORT [Export OK on TDRF2]\nMay 11 10:19:05 csvmg226 configurator: EXPORT [End of csv generation]\nMay 11 10:19:05 csvmg226 configurator: EXPORT [CHECK CONFIG and EXPORT TO FRONT SYSTEMS -> OK]\nMay 11 10:19:05 csvmg226 configurator_drf[18338]: NIV1: main() commande: <./export.sh base bfr2501 TDB2 3307 N161214_K2R_tmp N161214_K2R /home/ultracomp/HALYSultracomp/own/system_halys.conf> executed\nMay 11 10:19:05 csvmg226 configurator_drf[18338]: NIV0: [CAM PROCESS]: STOP EXPORTATION CONFIG : SUCCESS (8182 ms) !\nMay 11 10:19:05 csvmg226 configurator_drf[18338]: NIV0: [CAM PROCESS]: END DRF PROCESSING FILE </opt/update/xmlfiles_drf/2019_05_11_12-18-10_DRF_FULL.xml> : SUCCESS (52401 ms) !\n"
                 },
                 {
+                    "id": "TEST2",
+                    "function": "TEST",
+                    "order": 3,
+                    "orchestration": "secondary",
                     "verdict": "Successful",
                     "dnsResults": [
                         {
@@ -163,39 +174,160 @@ const Detail = (props) => {
                     ]
                 },
                 {
+                    "id": "DRF_LAU1",
+                    "function": "DRF",
+                    "order": 4,
+                    "orchestration": "primary",
                     "verdict": "Successful",
                     "code": 0,
                     "serviceName": "orchestration-service-mg-bau1",
                     "server": "csvdf220",
-                    "function": "DRF",
                     "standardOutput": null,
                     "errorOutput": "Warning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\nWarning: Using a password on the command line interface can be insecure.\n",
                     "verificationFile": "/home/ultracomp/etc/ListeBlanche.csv",
                     "verificationTimestampBefore": "11.05.2019 10:19:02",
                     "verificationTimestampAfter": "11.05.2019 10:19:57",
                     "configuratorLog": "8 csvmg226 nb1[18895]: Stopped\nMay 11 10:19:58 csvmg226 configurator: EXPORT [End of csv generation of ALL configuration files (csv)]\nMay 11 10:19:59 csvmg226 configurator: EXPORT [Check config -> OK]\nMay 11 10:19:59 csvmg226 configurator: EXPORT [Config exported locally]\nMay 11 10:20:00 csvmg226 configurator: EXPORT [Export OK on TDRF2]\nMay 11 10:20:00 csvmg226 configurator: EXPORT [End of csv generation]\nMay 11 10:20:00 csvmg226 configurator: EXPORT [CHECK CONFIG and EXPORT TO FRONT SYSTEMS -> OK]\nMay 11 10:20:00 csvmg226 configurator_drf[18769]: NIV1: main() commande: <./export.sh base bfr2501 TDB2 3307 N161214_K2R_tmp N161214_K2R /home/ultracomp/HALYSultracomp/own/system_halys.conf> executed\nMay 11 10:20:00 csvmg226 configurator_drf[18769]: NIV0: [CAM PROCESS]: STOP EXPORTATION CONFIG : SUCCESS (7985 ms) !\nMay 11 10:20:00 csvmg226 configurator_drf[18769]: NIV0: [CAM PROCESS]: END DRF PROCESSING FILE </opt/update/xmlfiles_drf/2019_05_11_12-19-06_DRF_FULL.xml> : SUCCESS (51326 ms) !\n"
+                },
+                {
+                    "id": "TEST3",
+                    "function": "TEST",
+                    "order": 5,
+                    "orchestration": "primary",
+                    "verdict": "Successful",
+                    "dnsResults": [
+                        {
+                            "verdict": "Successful",
+                            "host": "csvns228",
+                            "request": "6B B0 00 00 00 01 00 00 00 00 00 00 08 69 6E 74 65 72 6E 65 74 06 6D 6E 63 30 39 36 06 6D 63 63 32 32 38 04 67 70 72 73 00 00 01 00 01 ",
+                            "answer": "6B B0 84 00 00 01 00 01 00 02 00 00 08 69 6E 74 65 72 6E 65 74 06 6D 6E 63 30 39 36 06 6D 63 63 32 32 38 04 67 70 72 73 00 00 01 00 01 C0 0C 00 01 00 01 00 00 0E 10 00 04 C2 96 C7 5C C0 15 00 02 00 01 00 00 0E 10 00 10 03 31 39 34 03 31 35 30 03 31 39 39 02 31 32 00 C0 15 00 02 00 01 00 00 0E 10 00 10 03 31 39 34 03 31 35 30 03 31 39 39 02 31 31 00 ",
+                            "error": null,
+                            "ipaddress": "194.150.199.92",
+                            "lookup": "internet.mnc096.mcc228.gprs.",
+                            "classname": "IN"
+                        },
+                        {
+                            "verdict": "Successful",
+                            "host": "csvns229",
+                            "request": "D5 98 00 00 00 01 00 00 00 00 00 00 08 69 6E 74 65 72 6E 65 74 06 6D 6E 63 30 39 36 06 6D 63 63 32 32 38 04 67 70 72 73 00 00 01 00 01 ",
+                            "answer": "D5 98 84 00 00 01 00 01 00 02 00 00 08 69 6E 74 65 72 6E 65 74 06 6D 6E 63 30 39 36 06 6D 63 63 32 32 38 04 67 70 72 73 00 00 01 00 01 C0 0C 00 01 00 01 00 00 0E 10 00 04 C2 96 C7 5C C0 15 00 02 00 01 00 00 0E 10 00 10 03 31 39 34 03 31 35 30 03 31 39 39 02 31 31 00 C0 15 00 02 00 01 00 00 0E 10 00 10 03 31 39 34 03 31 35 30 03 31 39 39 02 31 32 00 ",
+                            "error": null,
+                            "ipaddress": "194.150.199.92",
+                            "lookup": "internet.mnc096.mcc228.gprs.",
+                            "classname": "IN"
+                        }
+                    ],
+                    "gtpEchoResult": [
+                        {
+                            "verdict": "Successful",
+                            "host": "185.47.24.193",
+                            "request": "32 01 00 04 00 00 00 00 00 01 00 00 ",
+                            "answer": "32 02 00 06 00 00 00 00 00 01 00 00 0E 84 ",
+                            "error": null
+                        },
+                        {
+                            "verdict": "Successful",
+                            "host": "91.221.82.121",
+                            "request": "32 01 00 04 00 00 00 00 00 01 00 00 ",
+                            "answer": "32 02 00 06 00 00 00 00 00 01 00 00 0E 84 ",
+                            "error": null
+                        },
+                        {
+                            "verdict": "Successful",
+                            "host": "195.211.13.120",
+                            "request": "32 01 00 04 00 00 00 00 00 01 00 00 ",
+                            "answer": "32 02 00 06 00 00 00 00 00 01 00 00 0E 84 ",
+                            "error": null
+                        },
+                        {
+                            "verdict": "Successful",
+                            "host": "185.47.24.193",
+                            "request": "32 01 00 04 00 00 00 00 00 01 00 00 ",
+                            "answer": "32 02 00 06 00 00 00 00 00 01 00 00 0E 84 ",
+                            "error": null
+                        },
+                        {
+                            "verdict": "Successful",
+                            "host": "91.221.82.121",
+                            "request": "32 01 00 04 00 00 00 00 00 01 00 00 ",
+                            "answer": "32 02 00 06 00 00 00 00 00 01 00 00 0E 84 ",
+                            "error": null
+                        },
+                        {
+                            "verdict": "Successful",
+                            "host": "195.211.13.120",
+                            "request": "32 01 00 04 00 00 00 00 00 01 00 00 ",
+                            "answer": "32 02 00 06 00 00 00 00 00 01 00 00 0E 84 ",
+                            "error": null
+                        }
+                    ]
                 }
             ]
         }
-
-
-
     )
 
-    let nodes =  details.resultList.map((node) => {
-
-        return (
-            <div className="Detail" >
-               <p>Verdict is {node.verdict}</p>
-                <p>Code is {node.code}</p>
-            </div>
-        );
+    details.resultList.sort((a, b) => {
+        return a < b ? -1 : 1;
 
     });
 
 
+    let testDictionary = [];
+    let secondaryDictionary = [];
+    let primaryDictionary = [];
+    for (let index = 0; index < details.resultList.length; index++) {
+        let node = details.resultList[index];
+        if (node.function === "TEST") {
+            testDictionary[node.id] = (
+                <div className="Detail">
+                    <p>Id is {node.id}</p>
+                    <p>Verdict is {node.verdict}</p>
+                </div>
+            )
+        }
+
+        if (node.function !== "TEST" && node.orchestration === "secondary") {
+            secondaryDictionary.push(
+                <div className="Detail" key={node.id}>
+                    <p>Id is {node.id}</p>
+                    <p>Verdict is {node.verdict}</p>
+                    <p>Code is {node.code}</p>
+                </div>
+            );
+        }
+
+        if (node.function !== "TEST" && node.orchestration === "primary") {
+            primaryDictionary.push(
+                <div className="Detail" key={node.id}>
+                    <p>Id is {node.id}</p>
+                    <p>Verdict is {node.verdict}</p>
+                    <p>Code is {node.code}</p>
+                </div>
+            );
+        }
+    }
+
+
     return (
-    <div className="Details">{nodes}</div>
+        <div>
+            <div className="Details">
+                <p>Secondary Nodes Orchestration</p>
+                {testDictionary["TEST1"]}
+                {secondaryDictionary}
+                {testDictionary["TEST2"]}
+            </div>
+            <div></div>
+            <div className="Details">
+                <p>Secondary Nodes Orchestration</p>
+                {primaryDictionary}
+                {testDictionary["TEST3"]}
+            </div>
+            <div className="Details">
+                <p>hallo </p>
+                <p>velo </p>
+                <p>fahrer</p>
+            </div>
+        </div>
     );
 }
 
--- orchestration/src/containers/App.js
+++ orchestration/src/containers/App.js
@@ -2,12 +2,36 @@ import React, {Component} from 'react';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import Detail from '../components/Detail/Detail';
+import MaterialTable from 'material-table';
 
 class App extends Component {
     render() {
         return (
+
             <BrowserRouter>
                 <div className="App">
+
+                    <MaterialTable
+                        title="Basic Filtering Preview"
+                        columns={[
+                            { title: 'Name', field: 'name' },
+                            { title: 'Surname', field: 'surname' },
+                            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+                            {
+                                title: 'Birth Place',
+                                field: 'birthCity',
+                                lookup: { 34: 'Istanbul', 63: 'Sanliurfa' },
+                            },
+                        ]}
+                        data={[
+                            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+                        ]}
+                        options={{
+                            filtering: true
+                        }}
+                    />
+
                     <Detail/>
                 </div>
             </BrowserRouter>
