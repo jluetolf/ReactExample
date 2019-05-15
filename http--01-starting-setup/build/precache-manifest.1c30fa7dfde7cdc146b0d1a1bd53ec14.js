821a56b6a6e267000d7c6e320316bfeadd834ab4
Joerg Luetolf <joerg.luetolf@comfone.com>
2019-05-15 11:06:06 +0200

    -

---
+++ http--01-starting-setup/build/precache-manifest.1c30fa7dfde7cdc146b0d1a1bd53ec14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0a617ad762e0b125947b55eb612e85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cc51833a08671375e66",
+    "url": "/static/css/main.288f8c9c.chunk.css"
+  },
+  {
+    "revision": "e25236e49ff7074ac269",
+    "url": "/static/js/2.b3329a45.chunk.js"
+  },
+  {
+    "revision": "1cc51833a08671375e66",
+    "url": "/static/js/main.d9f361ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
