d192cadd8d47a0c0856474549c44e44c68f25ed6
Joerg Luetolf <joerg_luetolf@hotmail.com>
2019-05-26 12:16:43 +0200

    -

--- orchestration/src/components/FullOrchestrationRow/FullOrchestrationRow.js
+++ orchestration/src/components/FullOrchestrationRow/FullOrchestrationRow.js
@@ -7,46 +7,95 @@ import Cluster from "../Cluster/Cluster";
 
 const FullOrchestrationRow = (props) => {
 
-    let testDictionary = [];
-    let secondaryDictionary = [];
-    let primaryDictionary = [];
+
+    let secondaryListDRF = [];
+    let secondaryListSS7F = [];
+    let secondaryListDSF = [];
+    let secondaryListDNSF = [];
+    let primaryListDRF = [];
+    let primaryListSS7F = [];
+    let primaryListDSF = [];
+    let primaryListDNSF = [];
+
+
+    let secondaryNodeList = [];
+    let primaryNodeList = []
+    let testNodeDictionary = [];
 
     for (let index = 0; index < props.orchestration.nodes.length; index++) {
         let node = props.orchestration.nodes[index];
         if (node.cluster_type === "secondary") {
-            secondaryDictionary[node.id] = (
-                <Node orchestrationNode={node} key={node.id}/>
-            )
+            switch (node.node_type) {
+                case "DRF":
+                    secondaryListDRF.push(node);
+                    break;
+                case "SS7F":
+                    secondaryListSS7F.push(node);
+                    break;
+                case "DSF":
+                    secondaryListDSF.push(node);
+                    break;
+                case "DNSF":
+                    secondaryListDNSF.push(node);
+                    break;
+                default:
+                    throw new Error(`An error in FullOrchestrationRow: Node type ${node.node_type} is not known`);
+            }
         }
         if (node.cluster_type === "primary") {
-            primaryDictionary[node.id] = (
-                <Node orchestrationNode={node} key={node.id}/>
-            )
+            switch (node.node_type) {
+                case "DRF":
+                    primaryListDRF.push(node);
+                    break;
+                case "SS7F":
+                    primaryListSS7F.push(node);
+                    break;
+                case "DSF":
+                    primaryListDSF.push(node);
+                    break;
+                case "DNSF":
+                    primaryListDNSF.push(node);
+                    break;
+                default:
+                    throw new Error(`An error in FullOrchestrationRow: Node type ${node.node_type} is not known`);
+            }
         }
     }
 
+    secondaryNodeList.push(<Node orchestrationNodes={secondaryListDRF} nodeType="DRF" key="DRF"/>);
+    secondaryNodeList.push(<Node orchestrationNodes={secondaryListSS7F} nodeType="SS7F" key="SS7F"/>);
+    secondaryNodeList.push(<Node orchestrationNodes={secondaryListDSF} nodeType="DSF" key="DSF"/>);
+    secondaryNodeList.push(<Node orchestrationNodes={secondaryListDNSF} nodeType="DNSF" key="DNSF"/>);
+
+    primaryNodeList.push(<Node orchestrationNodes={primaryListDRF} nodeType="DRF" key="DRF"/>);
+    primaryNodeList.push(<Node orchestrationNodes={primaryListSS7F} nodeType="SS7F" key="SS7F"/>);
+    primaryNodeList.push(<Node orchestrationNodes={primaryListDSF} nodeType="DSF" key="DSF"/>);
+    primaryNodeList.push(<Node orchestrationNodes={primaryListDNSF} nodeType="DNSF" key="DNSF"/>);
+
+
     for (let index = 0; index < props.orchestration.tests.length; index++) {
         let test = props.orchestration.tests[index];
-        testDictionary[test.test_type] = (
+        testNodeDictionary[test.test_type] = (
             <Test orchestrationNode={test} key={test.id}/>
         )
     }
     return (
         <div className="FullOrchestrationRow">
             <OrchestrationType name={props.orchestration.orchestrationType}
-                               date={props.orchestration.orchestration_date}/>
+                               date={props.orchestration.orchestration_date}
+                               user={props.orchestration.orchestration_user} />
             <div className="Orchestration">
                 <div className="OrchestrationCluster">
                     <Cluster cluster="Secondary"/>
-                    {testDictionary[1]}
-                    {secondaryDictionary}
-                    {testDictionary[2]}
+                    {testNodeDictionary[1]}
+                    {secondaryNodeList}
+                    {testNodeDictionary[2]}
                 </div>
                 <div className="OrchestrationCluster">
                     <Cluster cluster="Primary"/>
-                    {testDictionary[2]}
-                    {primaryDictionary}
-                    {testDictionary[3]}
+                    {testNodeDictionary[2]}
+                    {primaryNodeList}
+                    {testNodeDictionary[3]}
                 </div>
             </div>
         </div>
--- orchestration/src/components/Node/Node.js
+++ orchestration/src/components/Node/Node.js
@@ -3,32 +3,52 @@ import './Node.css';
 import green from '../../resources/green.png';
 import red from '../../resources/red.png';
 import yellow from '../../resources/yellow.png';
+import grey from '../../resources/grey.png';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 const Node = (props) => {
 
-    let bullet = red;
+    let bullet = grey;
+    const serverList = [];
 
-    switch (props.orchestrationNode.verdict) {
-        case 'Successful':
-            bullet = green;
-            break;
-        case 'Pending':
-            bullet = yellow;
-            break;
-        default:
-            bullet = red;
-            break;
+    for (let index = 0; index < props.orchestrationNodes.length; index++) {
+        let node = props.orchestrationNodes[index];
+
+        serverList.push(node.server);
+
+        switch (node.verdict) {
+            case 'Successful':
+                bullet = green;
+                break;
+            case 'Pending':
+                bullet = yellow;
+                break;
+            case 'Failed':
+                bullet = red;
+                break;
+            default:
+                throw new Error(`An error in Node: Verdict  ${node.verdict} is not known`);
+        }
     }
+
+
+
+    let servers = " Server";
+    if (props.orchestrationNodes.length > 1) {
+        servers = " Servers";
+    }
+
+     servers = props.orchestrationNodes.length + servers;
+
 
 
     return (
         <div className="Node">
             <div className="FirstRow">
                 <img id="bullet" src={bullet} alt="{green}"></img>
-                <div id="node_type">{props.orchestrationNode.node_type}</div>
-                <div id="cluster_name">{props.orchestrationNode.cluster_name}</div>
+                <div id="node_type">{props.nodeType}</div>
+                <div id="cluster_name" title={serverList}>{servers}</div>
             </div>
             <div className="SecondRow">
                 <button type="button" className="btn info">Orchestrate</button>
